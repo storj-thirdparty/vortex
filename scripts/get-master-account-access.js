d72a8ca8ce27acb5812045397424e833eb691abb
Monty Anderson <work@montyanderson.net>
2020-11-25 18:10:27 +0000

    stargate and access code

---
+++ scripts/get-master-account-access.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const axios = require('axios');
+const config = require('../config.json');
+const generateAccess = require('../lib/generateAccess.js');
+const getStargateCredentials = require('../lib/getStargateCredentials.js');
+
+(async () => {
+
+	const access = await generateAccess();
+
+	config.masterAccount = {
+		...config.masterAccount,
+		...(await getStargateCredentials(access))
+	};
+
+	fs.writeFileSync(`${__dirname}/../config.json`, JSON.stringify(config, null, '\t'));
+
+})();
