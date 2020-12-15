d121f13f2182fcb4943044c1f51e534ac6d8bcb6
Tome Boshevski <tome@boshevski.com>
2020-12-15 20:27:54 +0100

    Add mascot

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -19,5 +19,7 @@
 		<h1 class="hero-title text-white">Decentralized Cloud Storage is Here</h1>
 
 		<p class="description my-4 text-white">Get safer, faster, decentralized object storage at a fraction of the cost. Experience it for yourself and start with 1 TB free when you sign up.</p>
+
+		<img src="@/assets/mascot.png" alt="Mascot" class="w-100">
 	</div>
 </template>
