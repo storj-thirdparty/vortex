567a2522b1707283f4d94872086971833a30ac55
Tome Boshevski <tome@boshevski.com>
2020-12-17 17:25:33 +0100

    Add back mascot

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,6 +5,6 @@
 
 		<p class="description my-4 text-white">Get safer, faster, decentralized object storage at a fraction of the cost. Experience it for yourself and start with 1 TB free when you sign up.</p>
 
-		<!--<img src="@/assets/mascot.png" alt="Mascot" class="w-100 mt-4">-->
+		<img src="@/assets/mascot.png" alt="Mascot" class="w-100 mt-4">
 	</div>
 </template>
