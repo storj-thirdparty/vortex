5e22e75503687a6ff3de3e38a86f80aac2560509
Monty Anderson <work@montyanderson.net>
2021-01-05 14:45:43 +0000

    userJson -> data

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -149,7 +149,7 @@ export default {
 				'lastAuditUpload',
 				'lastAuditDownload'
 			]) {
-				this.userJson[key] = prettyBytes(this.userJson[key]);
+				data[key] = prettyBytes(data[key]);
 			}
 
 			this.userJson = data;
