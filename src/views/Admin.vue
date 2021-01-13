8eb7562def11b7e792046b566fc4789df5079595
Tome Boshevski <tome@boshevski.com>
2021-01-13 10:49:54 +0100

    Admin user status col update

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -33,7 +33,8 @@
 								<thead>
 									<tr>
 										<th scope="col" class="pl-2 text-center">ID</th>
-										<th scope="col" class="pl-4">Email</th>
+										<th scope="col"></th>
+										<th scope="col">Email</th>
 										<th scope="col">Created</th>
 										<th scope="col">Last Login</th>
 										<th scope="col">Storage</th>
@@ -61,9 +62,8 @@
 												 d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
 											</svg>
 
-											{{user.email}}
-
 										</td>
+										<td class="align-middle">{{user.email}}</td>
 										<td class="align-middle">{{user.createTime | toNiceDate}}</td>
 										<td class="align-middle">{{user.lastLoginTime | toNiceDate}}</td>
 										<td class="align-middle">{{user.filesUsed}} files, {{user.bytesUsed | prettyBytes}}</td>
