8351c423b86f410c56ff78778d97edf044fbde63
gabedealmeida <hello@gabrieldealmeida.com>
2021-02-03 09:51:13 -0500

    Fixed the scrolling issue when interacting with the dropdown.

--- src/components/FileEntry.vue
+++ src/components/FileEntry.vue
@@ -73,27 +73,27 @@
 					</svg>
 				</button>
 			  <div class="dropdown-menu shadow show" v-if="dropdownOpen">
-			    <a class="dropdown-item action p-3" href="#" v-on:click="$emit('download'); dropdownOpen = false;">
+			    <button class="dropdown-item action p-3" href="#" v-on:click="$emit('download'); dropdownOpen = false;">
 						<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-cloud-download mr-2 ml-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 							<path fill-rule="evenodd"
 							 d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z" />
 							<path fill-rule="evenodd" d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z" />
 						</svg>
 						Download
-					</a>
-			    <a class="dropdown-item action p-3" href="#" v-on:click="share; dropdownOpen = false;">
+					</button>
+			    <button class="dropdown-item action p-3" href="#" v-on:click="share; dropdownOpen = false;">
 						<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-link-45deg mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 							<path d="M4.715 6.542L3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.001 1.001 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
 							<path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z" />
 						</svg>
 						{{shareText}}
-					</a>
-			    <a v-if="!deleteConfirmation" class="dropdown-item action p-3" href="#" v-on:click="handleDelete">
+					</button>
+			    <button v-if="!deleteConfirmation" class="dropdown-item action p-3" href="#" v-on:click="deleteConfirmation = true">
 						<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-x mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 							<path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
 						</svg>
 						Delete
-					</a>
+					</button>
           <a v-else class="dropdown-item action p-3" href="#" v-on:click="$emit('delete'); closeDropdown()">
 						<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-x mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 							<path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
@@ -165,10 +165,6 @@ export default {
     closeDropdown() {
       this.deleteConfirmation = false;
     },
-    handleDelete(event) {
-      event.preventDefault();
-      this.deleteConfirmation = true
-    }
 	}
 }
 </script>
