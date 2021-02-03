e38212c9866e30775bd499888c2e979dd02a106a
Shawn Wilkinson <me@super3.org>
2021-02-03 12:06:29 -0500

    Add Icon

--- src/components/FileBrowser.vue
+++ src/components/FileBrowser.vue
@@ -81,7 +81,12 @@
 			</thead>
 			<tbody>
 				<tr v-for="file in filesUploading" scope="row">
-					<td class="upload-text">{{filename(file)}}</td>
+					<td class="upload-text">
+						<span>
+						<svg width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="bi bi-file-earmark ml-2 mr-1"><path d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"></path><path d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z"></path></svg>
+						 {{filename(file)}}
+						</span>
+					</td>
 					<td></td>
 					<td></td>
 					<td>
