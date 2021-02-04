0658daecf4f3308ebc827c3c456b0975ee8147b4
gabedealmeida <hello@gabrieldealmeida.com>
2021-02-04 15:39:52 -0500

    Added right click functionality to files.

--- src/components/FileEntry.vue
+++ src/components/FileEntry.vue
@@ -42,7 +42,7 @@
 </style>
 
 <template>
-<tr scope="row">
+<tr scope="row" v-on:click.right="toggleDropdown">
 	<td class="w-50" data-ls-disabled>
 		<span v-if="file.type === 'folder'">
 			<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-folder ml-2 mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -74,14 +74,14 @@
 		<div v-if="file.type === 'file'">
 			<div class="dropleft">
 				<div v-if="loadingSpinner" class="spinner-border" role="status"></div>
-				<button v-else class="btn btn-white btn-actions" type="button" aria-haspopup="true" aria-expanded="false" v-on:click="toggleDropdown">
+				<button v-else class="btn btn-white btn-actions" type="button" aria-haspopup="true" aria-expanded="false">
 					<svg width="4" height="16" viewBox="0 0 4 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d="M3.2 1.6C3.2 2.48366 2.48366 3.2 1.6 3.2C0.716344 3.2 0 2.48366 0 1.6C0 0.716344 0.716344 0 1.6 0C2.48366 0 3.2 0.716344 3.2 1.6Z" fill="#7C8794" />
 						<path d="M3.2 8C3.2 8.88366 2.48366 9.6 1.6 9.6C0.716344 9.6 0 8.88366 0 8C0 7.11634 0.716344 6.4 1.6 6.4C2.48366 6.4 3.2 7.11634 3.2 8Z" fill="#7C8794" />
 						<path d="M1.6 16C2.48366 16 3.2 15.2837 3.2 14.4C3.2 13.5163 2.48366 12.8 1.6 12.8C0.716344 12.8 0 13.5163 0 14.4C0 15.2837 0.716344 16 1.6 16Z" fill="#7C8794" />
 					</svg>
 				</button>
-				<div class="dropdown-menu shadow show" v-if="dropdownOpen">
+				<div class="dropdown-menu shadow show" v-if="dropdownOpen" v-on:click.right="toggleDropdown">
 					<button class="dropdown-item action p-3" v-on:click="download">
 						<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-cloud-download mr-2 ml-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 							<path fill-rule="evenodd"
@@ -167,6 +167,7 @@ export default {
 			this.shareText = 'Copy Link';
 		},
 		toggleDropdown(event) {
+			event.preventDefault();
 			event.stopPropagation();
 
 			if (this.$store.state.openedDropdown === this.file.Key) {
