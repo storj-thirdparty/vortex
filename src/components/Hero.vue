854e4025c36ad7ca9767ac3625c2b7873c416080
Tome Boshevski <tome@boshevski.com>
2021-02-06 10:21:48 +0100

    Update homepage

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,10 +9,12 @@
 <template>
 	<div class="hero">
 
-		<h1 class="hero-title text-white">Decentralized Cloud Storage is Here</h1>
+		<img src="@/assets/storj-duplicati.svg" alt="Storj + Duplicati" width="140">
+
+		<h2 class="hero-title text-white mt-4">Get 1 TB Free When You Try Storj With Duplicati</h2>
 
 		<p class="description my-4 text-white">Get safer, faster, decentralized object storage at a fraction of the cost. Experience it for yourself and start with 1 TB free when you sign up.</p>
 
-		<img src="@/assets/mascot.png" alt="Mascot" class="w-100 mt-4 mascot">
+		<!-- <img src="@/assets/mascot.png" alt="Mascot" class="w-100 mt-4 mascot"> -->
 	</div>
 </template>
