6356978774c2abc9330b80fe50a3aaa69cb207b1
Monty Anderson <work@montyanderson.net>
2021-02-08 14:25:10 +0000

    change tab name

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -14,11 +14,12 @@
 			</li>
 			<li class="nav-item">
 				<router-link to="/dashboard/apps" class="nav-link" v-bind:class="tabStyles('/dashboard/apps')">
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-window" viewBox="0 0 16 16">
-						<path d="M2.5 4a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
-						<path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm13 2v2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zM2 14a1 1 0 0 1-1-1V6h14v7a1 1 0 0 1-1 1H2z" />
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-arrow-up" viewBox="0 0 16 16">
+						<path fill-rule="evenodd" d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z" />
+						<path
+						 d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
 					</svg>
-					S3 &amp; Apps
+					Backup with Duplicati
 				</router-link>
 			</li>
 			<li class="nav-item white-text">
@@ -70,8 +71,10 @@ export default {
 	},
 	watch: {
 		isLoggedIn(loggedIn) {
-			if(loggedIn === false) {
-				this.$router.push({path:'/'});
+			if (loggedIn === false) {
+				this.$router.push({
+					path: '/'
+				});
 			}
 		}
 	},
@@ -81,8 +84,10 @@ export default {
 	async created() {
 		await this.$store.dispatch('passiveLogin');
 
-		if(this.isLoggedIn === false) {
-			this.$router.push({path:'/'});
+		if (this.isLoggedIn === false) {
+			this.$router.push({
+				path: '/'
+			});
 		}
 
 		this.$store.dispatch('getUsage');
