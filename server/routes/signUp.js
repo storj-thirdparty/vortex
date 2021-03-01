4be41ffb3dec81b07da247ce69d19d8d0d9fb580
Gabriel De Almeida <hello@gabrieldealmeida.com>
2021-03-01 11:04:29 -0500

    Refactored the steps.

--- server/routes/signUp.js
+++ server/routes/signUp.js
@@ -17,12 +17,12 @@ module.exports = async function(ctx, next) {
 		throw new ApiError("Not a valid email address.");
 	}
 
-	if (!password) {
-		throw new ApiError("Not a valid password.");
-	}
-
 	if (email.length > 100) {
 		throw new ApiError("Email too long.");
+	}
+
+	if (!password) {
+		throw new ApiError("Not a valid password.");
 	}
 
 	if (password.length < 8) {
